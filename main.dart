42ae6971405ff5863edccbdfb61ccd869a113df6
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-06-04 11:46:25 +0000

    $variable ${variable} ${variable.function()}

--- main.dart
+++ main.dart
@@ -3,4 +3,8 @@ void main() {
   test = "test";
   test = 1;
   test = true;
+  var name = 'nico';
+  print("Hi! My name is $name.toUpperCase() !");
+  print(name.toUpperCase());
+  print("Hi! My name is ${name.toUpperCase()} !");
 }
