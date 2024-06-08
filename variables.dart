8bb59957b3268d046fbbbdd54a2839826176903f
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-06-08 10:36:01 +0000

    variables

--- variables.dart
+++ variables.dart
@@ -5,4 +5,21 @@ void main() {
   late final String test;
   test = "later";
   print(test);
+  const birthday = 0718;
+  print(birthday);
+
+  var x;
+  x=1;
+  int i = 12;
+  x='x';
+  dynamic anything;
+  anything = 'String';
+  anything = 1;
+  anything = true;
+
+  String? temp1 = 'temp1';
+  temp1 = null;
+  temp1?.isEmpty;
+  print(temp1);
+  print(temp1?.isEmpty);
 }
