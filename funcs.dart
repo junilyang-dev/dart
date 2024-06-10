e4fcad1b1a089effd590db6049086ef5bc56283d
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-06-10 12:24:57 +0000

    named arguments

--- funcs.dart
+++ funcs.dart
@@ -1,5 +1,5 @@
-void sayHello(String name){
-  print('Hello $name, nice to meet you!');
+void sayHello(String name, int age, String country){
+  print('Hello $name, you are $age, and you come from $country');
 }
 String sayHello1(String name){
   return 'Hello $name, nice to meet you!';
@@ -8,10 +8,24 @@ String sayHello2(String name) => 'Hello $name, nice to meet you!';
 
 num plus(num a, num b) => a+b;
 
+//named arguments
+void sayHello3({String name='anon', int age=99, String country='wakanda'}){
+  print('Hello $name, you are $age, and you come from $country');
+}
+String sayHello4({
+  required name,
+  required age,
+  required country
+  }){
+  return 'Hello $name, you are $age, and you come from $country';
+}
 
 void main(){
-  sayHello('arum');
+  sayHello('arum',9,'korea');
   print(sayHello1('junil'));
   print(sayHello2('huchu'));
   print(plus(1,2));
+  sayHello3(age:9,country:'korea',name:'named');
+  sayHello3();
+  print(sayHello4(age:9,country:'korea',name:'junil'));
 }
